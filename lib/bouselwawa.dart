13fb8e449921d92675f58c6abc1c2a98e4384d91
mahmoudhatems <113213094+mahmoudhatems@users.noreply.github.com>
2024-11-03 22:43:57 +0200

    Add color constants and clean up Bouselwawa widget

--- lib/bouselwawa.dart
+++ lib/bouselwawa.dart
@@ -6,7 +6,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 class Bouselwawa extends StatelessWidget {
   final AppRouter appRouter ;
   const Bouselwawa({super.key, required this.appRouter});
-
+ 
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
--- lib/core/theming/colors.dart
+++ lib/core/theming/colors.dart
@@ -1,3 +1,16 @@
+import 'package:flutter/material.dart';
+
 class ColorsManager{
-  
+  static const Color primaryColorDarkTeal = Color(0xFF1C2A3A);
+  static const Color secondaryColor = Color(0xFF2C3E50);
+  static const Color gray = Color(0xFF9B9B9B);
+  static const Color textGray = Color(0xFF6B7280);
+  static const Color textTeal = Color(0xFF374151);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color mainBLue = Color(0xFF1C64F2);
+  static const Color black = Color(0xFF000000);
+  static const Color red = Color(0xFFE74C3C);
+  static const Color green = Color(0xFFA4CFC3);
+  static const Color backGroundGray = Color(0xFFE5E7EB);
+
 }
