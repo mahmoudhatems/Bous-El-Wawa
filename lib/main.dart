788244cbd89effadca5aef56544fa0746cb90679
mahmoudhatems <113213094+mahmoudhatems@users.noreply.github.com>
2025-04-03 05:08:52 +0200

    ✨Enhance app initialization: ensure screen size before setup and remove native splash screen for improved user experience.

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,15 @@ import 'package:bouselwawa/bouselwawa.dart';
 import 'package:bouselwawa/core/di/dependancy_injection.dart';
 import 'package:bouselwawa/core/routing/app_router.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await setupGetIt();
-  runApp(Bouselwawa(appRouter: AppRouter(),));
+
+  await Future.wait([ScreenUtil.ensureScreenSize(), setupGetIt()]);
+  runApp(Bouselwawa(
+    appRouter: AppRouter(),
+  ));
+  FlutterNativeSplash.remove();
 }
- 
